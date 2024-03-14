5414cad25341eb4d6a124a9f8571c654ae0504d5
WaifuPower <pavleti06@gmail.com>
2024-03-14 23:40:56 +0200

    Nav Drawer finally working properly

---
+++ app/src/main/java/telkadjiite/hacktuesx/prilojenie/CreateEvents.java
@@ -0,0 +1,19 @@
+package telkadjiite.hacktuesx.prilojenie;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CreateEvents extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_events, container, false);
+    }
+}
---
+++ app/src/main/java/telkadjiite/hacktuesx/prilojenie/Profile.java
@@ -0,0 +1,27 @@
+package telkadjiite.hacktuesx.prilojenie;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class Profile extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
