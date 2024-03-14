0f8079deec51fe5491d6eac8300b17eeebfa3be9
Nick <64863561+NickProgrammerGaming@users.noreply.github.com>
2024-03-14 12:27:32 +0200

    Added login and register page

--- app/src/main/java/telkadjiite/hacktuesx/prilojenie/RegisterActivity.java
+++ app/src/main/java/telkadjiite/hacktuesx/prilojenie/RegisterActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,45 +13,44 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
-    EditText emailEditText;
-    EditText passwordEditText;
-    TextView forgotPassword;
-    TextView registerText;
-    Button loginButton;
+
+    EditText firstNameInput;
+    EditText lastNameInput;
+    EditText emailInput;
+    EditText passwordInput;
+    EditText passwordConfirmInput;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-       mAuth = FirebaseAuth.getInstance();
-       emailEditText = findViewById(R.id.emailInput);
-       passwordEditText = findViewById(R.id.passwordInput);
-       forgotPassword = findViewById(R.id.forgotPassword);
-       registerText = findViewById(R.id.registerText);
-       loginButton = findViewById(R.id.loginButton);
+        mAuth = FirebaseAuth.getInstance();
+        firstNameInput = findViewById(R.id.firstNameInput);
+        lastNameInput = findViewById(R.id.lastNameInput);
+        emailInput = findViewById(R.id.emailInputRegister);
+        passwordInput = findViewById(R.id.emailInputRegister);
+        passwordConfirmInput = findViewById(R.id.passwordConfirmInput);
+        registerButton = findViewById(R.id.registerButton);
 
-       loginButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               // Login
-           }
-       });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Register
+            }
+        });
 
-        
     }
-
-
 }
